301ee1ea6a61a833b13501cf71028a89b5aa996e
RANJITHMANI N <144892468+ranjithnphp@users.noreply.github.com>
2023-09-16 12:05:05 +0530

    Updated demoloops.php

--- demoloops.php
+++ demoloops.php
@@ -1,1 +1,25 @@
-file name is - demo02.txt
+<?php
+
+for($i=1;$i<10;$i++){
+echo $i;
+}
+echo "<br>";
+
+$j=10;
+while ($j<20){
+	echo $j;
+	$j++;
+}
+echo "<br>";
+
+
+$n=50;
+do{
+	echo $n;
+	$n++;
+
+}while($n<=70);
+
+?>
+
+
